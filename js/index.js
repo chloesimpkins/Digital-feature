1d822a5bf5459ea641a9a32a31e6018f4314049d
chloesimpkins <33317604+chloesimpkins@users.noreply.github.com>
2018-01-28 17:03:40 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    captions: true
+
+  });
+  });
+
+
+$(document).ready(function(){
+  $('.carousel_two').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    captions: true
+
+});
+});
